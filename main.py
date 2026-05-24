6c8b46831b8e3f0039de8e42e60187e86ea561fd
alucardyummy <dazaisagaz@gmail.com>
2026-05-24 11:00:30 -0300

    fix: usar pyOpenSSL puro sem cryptography

--- main.py
+++ main.py
@@ -37,34 +37,21 @@ def generate_cert():
     key_path  = os.path.join(cert_dir, 'key.pem')
     if os.path.exists(cert_path) and os.path.exists(key_path):
         return cert_path, key_path
-    from cryptography import x509
-    from cryptography.x509.oid import NameOID
-    from cryptography.hazmat.primitives import hashes, serialization
-    from cryptography.hazmat.primitives.asymmetric import rsa
-    import datetime
-    key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-    name = x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, u"localhost")])
-    cert = (
-        x509.CertificateBuilder()
-        .subject_name(name).issuer_name(name)
-        .public_key(key.public_key())
-        .serial_number(x509.random_serial_number())
-        .not_valid_before(datetime.datetime.utcnow())
-        .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=3650))
-        .add_extension(x509.SubjectAlternativeName([
-            x509.DNSName(u"localhost"),
-            x509.IPAddress(__import__('ipaddress').IPv4Address('127.0.0.1')),
-        ]), critical=False)
-        .sign(key, hashes.SHA256())
-    )
+    from OpenSSL import crypto
+    key = crypto.PKey()
+    key.generate_key(crypto.TYPE_RSA, 2048)
+    cert = crypto.X509()
+    cert.get_subject().CN = "localhost"
+    cert.set_serial_number(1)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(10 * 365 * 24 * 60 * 60)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(key)
+    cert.sign(key, 'sha256')
     with open(cert_path, 'wb') as f:
-        f.write(cert.public_bytes(serialization.Encoding.PEM))
+        f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
     with open(key_path, 'wb') as f:
-        f.write(key.private_bytes(
-            serialization.Encoding.PEM,
-            serialization.PrivateFormat.TraditionalOpenSSL,
-            serialization.NoEncryption()
-        ))
+        f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
     return cert_path, key_path
 
 
