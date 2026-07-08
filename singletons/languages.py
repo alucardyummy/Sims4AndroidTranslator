4dd8eec8ce73847ec9fcd893b8fd19d9c347fc39
Alucard <dazaisagaz@gmail.com>
2026-07-08 03:40:03 -0300

    Update built-in locales to remove unsupported entries
    Removed unsupported locales from the built-in language list based on game recognition.

--- singletons/languages.py
+++ singletons/languages.py
@@ -43,6 +43,11 @@ class Languages:
                 self.__codes[lang.code] = lang
 
     def __load_builtin(self):
+        # Apenas os 18 locales que o jogo de fato reconhece
+        # (confirmado pelo enum StringTableLocale do S4TK / stbl-locales.ts).
+        # POR_PT, THA_TH e UKR_UA foram removidos: não existem no jogo real,
+        # e gerar uma STBL com esses bytes de locale faz o jogo/S4TK não
+        # reconhecer o arquivo como locale nenhum.
         table = [
             ('ENG_US','0x00','en','EN'),('CHS_CN','0x01','zh-cn',''),
             ('CHT_CN','0x02','zh-tw','ZH'),('CZE_CZ','0x03','cs','CS'),
@@ -51,10 +56,9 @@ class Languages:
             ('GER_DE','0x08','de','DE'),('ITA_IT','0x0B','it','IT'),
             ('JPN_JP','0x0C','ja','JA'),('KOR_KR','0x0D','ko','KO'),
             ('NOR_NO','0x0E','no','NB'),('POL_PL','0x0F','pl','PL'),
-            ('POR_PT','0x10','pt','PT-PT'),('POR_BR','0x11','pt','PT-BR'),
+            ('POR_BR','0x11','pt','PT-BR'),
             ('RUS_RU','0x12','ru','RU'),('SPA_ES','0x13','es','ES'),
-            ('SWE_SE','0x15','sv','SV'),('THA_TH','0x16','th','TH'),
-            ('UKR_UA','0x17','uk','UA'),
+            ('SWE_SE','0x15','sv','SV'),
         ]
         for locale, code, google, deepl in table:
             lang = Language(locale, code, google, deepl)
